10786626b86949c421b03ee853ca42ba4c88fcd9
srib <peterf1a08@yahoo.com.hk>
2018-12-24 18:15:33 +0800

    lesson17: updating our status

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,19 +2,28 @@ import React from 'react'
 import { formatPrice } from '../helpers'
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index)
+  }
   render() {
     const { image, name, price, desc, status } = this.props.details
     // const image = this.props.details.image
     // const name = this.props.details.name
+    const isAvailable = status === 'available'
     return (
     <li className="menu-fish">
       <img src={image} alt={name} />
       <h3 className="fish-name">
         {name}
-        <span className="price">{formatPrice(price)}</span>
+        <span className="price">
+        {formatPrice(price)}
+        </span>
       </h3>
       <p>{ desc }</p>
-      <button>Add To Cart</button>
+      <button 
+      disabled={!isAvailable} onClick={this.handleClick}>
+      {isAvailable ? "Add To Order" : "Sold Out"}
+      </button>
     </li>
     )
   }
