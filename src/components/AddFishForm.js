503cd199d31cbb192067189964eabc963c2ab71f
srib <peterf1a08@yahoo.com.hk>
2018-12-24 11:30:00 +0800

    react learning

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AddFishForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input name="name" type="text" placeholder="Uame" />
+        <input name="price" type="text" placeholder="Price" />
+        <input name="status" type="text" placeholder="Status" />
+        <input name="desc" type="text" placeholder="Desc" />
+        <input name="image" type="text" placeholder="Image" />
+      </form>
+    )
+  }
+}
+
+export default AddFishForm
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Order extends React.Component {
+  render() {
+    return <div className="inventory">Order!!!</div>
+  }
+}
+
+export default Order
